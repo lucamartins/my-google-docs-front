b15fe01e5b79b34bab168ed42e5d0115e6155b39
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-07-19 16:26:32 -0300

    add ip

--- src/hooks/useWebSocketClient/useWebSocketClient.ts
+++ src/hooks/useWebSocketClient/useWebSocketClient.ts
@@ -1,7 +1,7 @@
 import { Client, messageCallbackType } from "@stomp/stompjs";
 import { useEffect, useMemo } from "react";
 
-const WEBSOCKET_URL = "ws://192.168.6.119:8080/gs-guide-websocket";
+const WEBSOCKET_URL = "ws://157.245.249.135:8080/gs-guide-websocket";
 
 const useWebSocketClient = () => {
   const client = useMemo(
